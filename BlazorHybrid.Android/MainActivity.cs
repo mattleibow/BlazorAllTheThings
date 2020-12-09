f4b5dc05d2753e05ba86ad6b17523de4e6200292
Matthew Leibowitz <mattleibow@live.com>
2020-12-09 02:31:34 +0200

    Added a hybrid sample

---
+++ BlazorHybrid.Android/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Microsoft.MobileBlazorBindings.WebView.Android;
+
+namespace BlazorHybrid.Droid
+{
+    [Activity(Label = "Blazor Hybrid", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            BlazorHybridAndroid.Init();
+
+            var fileProvider = new AssetFileProvider(Assets, "wwwroot");
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            LoadApplication(new App(fileProvider));
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
--- BlazorMobile.Android/MainActivity.cs
+++ BlazorMobile.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Android.OS;
 
 namespace BlazorMobile.Droid
 {
-    [Activity(Label = "BlazorMobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Blazor Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
